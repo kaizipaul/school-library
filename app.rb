c1c333e42420580ee1d41ced91d518b4cfbf427e
kaizipaul <kaizipaul@gmail.com>
2023-01-20 16:48:40 +0300

    Corrections per code review

--- app.rb
+++ app.rb
@@ -2,6 +2,9 @@ require_relative './student'
 require_relative './teacher'
 require_relative './rental'
 require_relative './book'
+require_relative './show_menu'
+require_relative './creation'
+require_relative 'listings'
 
 class App
   include ShowMenu
